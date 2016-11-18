c97c0705acc07e6cbf04a85f11ac2ecd449b26a4
Vladimir Parfinenko <vparfinenko@excelsior-usa.com>
2016-11-18 11:10:16 +0700

    Initial copy of helloworld

---
+++ GitAutoComp.cpp
@@ -0,0 +1,79 @@
+#include <plugin.hpp>
+#include "GitAutoCompLng.hpp"
+#include "version.hpp"
+#include <initguid.h>
+#include "guid.hpp"
+#include "GitAutoComp.hpp"
+
+static struct PluginStartupInfo Info;
+
+void WINAPI GetGlobalInfoW(struct GlobalInfo *Info)
+{
+	Info->StructSize=sizeof(struct GlobalInfo);
+	Info->MinFarVersion=FARMANAGERVERSION;
+	Info->Version=PLUGIN_VERSION;
+	Info->Guid=MainGuid;
+	Info->Title=PLUGIN_NAME;
+	Info->Description=PLUGIN_DESC;
+	Info->Author=PLUGIN_AUTHOR;
+}
+
+/*
+ Функция GetMsg возвращает строку сообщения из языкового файла.
+ А это надстройка над Info.GetMsg для сокращения кода :-)
+*/
+const wchar_t *GetMsg(int MsgId)
+{
+	return Info.GetMsg(&MainGuid,MsgId);
+}
+
+/*
+Функция SetStartupInfoW вызывается один раз, перед всеми
+другими функциями. Она передается плагину информацию,
+необходимую для дальнейшей работы.
+*/
+void WINAPI SetStartupInfoW(const struct PluginStartupInfo *psi)
+{
+	Info=*psi;
+}
+
+/*
+Функция GetPluginInfoW вызывается для получения информации о плагине
+*/
+void WINAPI GetPluginInfoW(struct PluginInfo *Info)
+{
+	Info->StructSize=sizeof(*Info);
+	Info->Flags=PF_EDITOR;
+	static const wchar_t *PluginMenuStrings[1];
+	PluginMenuStrings[0]=GetMsg(MTitle);
+	Info->PluginMenu.Guids=&MenuGuid;
+	Info->PluginMenu.Strings=PluginMenuStrings;
+	Info->PluginMenu.Count=ARRAYSIZE(PluginMenuStrings);
+}
+
+/*
+  Функция OpenPluginW вызывается при создании новой копии плагина.
+*/
+HANDLE WINAPI OpenW(const struct OpenInfo *OInfo)
+{
+	const wchar_t *MsgItems[]=
+	{
+		GetMsg(MTitle),
+		GetMsg(MMessage1),
+		GetMsg(MMessage2),
+		GetMsg(MMessage3),
+		GetMsg(MMessage4),
+		L"\x01",                      /* separator line */
+		GetMsg(MButton),
+	};
+
+	Info.Message(&MainGuid,           /* GUID */
+		NULL,
+		FMSG_WARNING|FMSG_LEFTALIGN,  /* Flags */
+		L"Contents",                  /* HelpTopic */
+		MsgItems,                     /* Items */
+		ARRAYSIZE(MsgItems),          /* ItemsNumber */
+		1);                           /* ButtonsNumber */
+
+	return NULL;
+}
---
+++ GitAutoCompLng.hpp
@@ -0,0 +1,10 @@
+enum {
+  MTitle,
+
+  MMessage1,
+  MMessage2,
+  MMessage3,
+  MMessage4,
+
+  MButton,
+};
