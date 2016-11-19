4346068c0f4f2d21ed31e0c5548e3e897a035a7f
Vladimir Parfinenko <vparfinenko@excelsior-usa.com>
2016-11-19 12:43:40 +0700

    Refactoring: support better replacement in cmdline + others

--- GitAutoComp.cpp
+++ GitAutoComp.cpp
@@ -13,16 +13,244 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
-using namespace std;
 
-static struct PluginStartupInfo Info;
+using namespace std;
 
 static wofstream logFile;
 
-const unsigned MAX_CMD_LINE = 4096;
+#pragma region Util functions
 
-void WINAPI GetGlobalInfoW(struct GlobalInfo *GInfo)
-{
+static string w2mb(wstring wstr) {
+    size_t mbstrSize = wstr.length() * 2 + 1;
+    char *mbstr = (char*)malloc(mbstrSize); // this is enough for all ;) FIXME
+    bool convertedOk = (wcstombs(mbstr, wstr.c_str(), mbstrSize) != (size_t)-1);
+    if (!convertedOk) {
+        logFile << "Cannot convert string \"" << wstr.c_str() << "\" to multi-byte string :(" << endl;
+        return string("");
+    }
+    string result = string(mbstr);
+    free(mbstr);
+    return result;
+}
+
+static wstring mb2w(string str) {
+    return wstring(str.begin(), str.end());
+}
+
+static bool StartsWith(const char *str, const char *prefix) {
+    return strstr(str, prefix) == str;
+}
+
+static bool StartsWith(const string &str, const string &prefix) {
+    return StartsWith(str.c_str(), prefix.c_str());
+}
+
+static string DropPrefix(const string &str, const string &prefix) {
+    assert(StartsWith(str, prefix));
+    return str.substr(prefix.length());
+}
+
+#pragma endregion
+
+#pragma region CmdLine struct and supporting functions
+
+typedef struct tCmdLine {
+    wstring line;
+    int curPos;
+    int selectionStart, selectionEnd;
+} CmdLine;
+
+typedef pair<int, int> Range;
+
+static int RangeLength(Range r) {
+    return r.second - r.first;
+}
+
+static Range GetSuggestedSuffixRange(const CmdLine &cmdLine) {
+    if (cmdLine.curPos == cmdLine.selectionEnd) {
+        assert(cmdLine.selectionStart >= 0);
+        return Range(cmdLine.selectionStart, cmdLine.selectionEnd);
+    } else {
+        // mimic potential location of suggested suffix
+        return Range(cmdLine.curPos, cmdLine.curPos);
+    }
+}
+
+static pair<int, int> GetUserPrefixRange(const CmdLine &cmdLine) {
+    int start = 0;
+    int end = cmdLine.curPos - RangeLength(GetSuggestedSuffixRange(cmdLine));
+    for (int i = end - 1; i >= 0; --i) {
+        if (iswspace(cmdLine.line.at(i))) {
+            start = i + 1;
+            break;
+        }
+    }
+    return pair<int, int>(start, end);
+}
+
+static wstring GetUserPrefix(const CmdLine &cmdLine) {
+    auto range = GetUserPrefixRange(cmdLine);
+    return cmdLine.line.substr(range.first, RangeLength(range));
+}
+
+static wstring GetSuggestedSuffix(const CmdLine &cmdLine) {
+    auto range = GetSuggestedSuffixRange(cmdLine);
+    return cmdLine.line.substr(range.first, RangeLength(range));
+}
+
+static Range ReplaceRange(CmdLine &cmdLine, Range range, const wstring &str) {
+    cmdLine.line.replace(range.first, RangeLength(range), str);
+    Range newRange = Range(range.first, range.first + (int)str.length());
+    return newRange;
+}
+
+static void ReplaceUserPrefix(CmdLine &cmdLine, const wstring &newPrefix) {
+    Range range = ReplaceRange(cmdLine, GetUserPrefixRange(cmdLine), newPrefix);
+    cmdLine.curPos = range.second;
+    cmdLine.selectionStart = -1;
+    cmdLine.selectionEnd = -1;
+}
+
+static void ReplaceSuggestedSuffix(CmdLine &cmdLine, const wstring &newSuffix) {
+    Range range = ReplaceRange(cmdLine, GetSuggestedSuffixRange(cmdLine), newSuffix);
+    cmdLine.curPos = range.second;
+    cmdLine.selectionStart = range.first;
+    cmdLine.selectionEnd = range.second;
+}
+
+#pragma endregion
+
+#pragma region Business logic :)
+
+static git_repository* OpenGitRepo(wstring dir) {
+    string dirForGit = w2mb(dir);
+    if (dirForGit.length() == 0) {
+        logFile << "Bad dir for Git: " << dir.c_str() << endl;
+        return nullptr;
+    }
+
+    git_repository *repo;
+    int error = git_repository_open_ext(&repo, dirForGit.c_str(), 0, nullptr);
+    if (error < 0) {
+        const git_error *e = giterr_last();
+        logFile << "libgit2 error " << error << "/" << e->klass << ": " << e->message << endl;
+        return nullptr;
+    }
+
+    return repo;
+}
+
+static void FilterReference(const char *ref, const char *currentPrefix, vector<string> &suitableRefs) {
+    if (StartsWith(ref, currentPrefix)) {
+        suitableRefs.push_back(string(ref));
+    }
+}
+
+static void FilterReferences(const char *ref, const char *currentPrefix, vector<string> &suitableRefs) {
+    const char *prefixes[] = { "refs/heads/", "refs/tags/", "refs/stash" };
+    const char *remotePrefix = "refs/remotes/";
+    for (int i = 0; i < sizeof(prefixes) / sizeof(prefixes[0]); ++i) {
+        if (StartsWith(ref, prefixes[i])) {
+            FilterReference(ref + strlen(prefixes[i]), currentPrefix, suitableRefs);
+            return;
+        }
+    }
+    if (StartsWith(ref, remotePrefix)) {
+        const char *remoteRef = ref + strlen(remotePrefix);
+        FilterReference(remoteRef, currentPrefix, suitableRefs);
+
+        const char *slashPtr = strchr(remoteRef, '/');
+        assert(slashPtr != nullptr);
+        FilterReference(slashPtr + 1, currentPrefix, suitableRefs);
+        return;
+    }
+    logFile << "DropRefPrefix: unexpected ref = " << ref << endl;
+}
+
+static void ObtainSuitableRefsByPrefix(git_repository *repo, string currentPrefix, vector<string> &suitableRefs) {
+    git_reference_iterator *iter = NULL;
+    int error = git_reference_iterator_new(&iter, repo);
+
+    git_reference *ref;
+    while (!(error = git_reference_next(&ref, iter))) {
+        FilterReferences(git_reference_name(ref), currentPrefix.c_str(), suitableRefs);
+    }
+    assert(error == GIT_ITEROVER);
+
+    // TODO: sort by date if settings
+    sort(suitableRefs.begin(), suitableRefs.end());
+    suitableRefs.erase(unique(suitableRefs.begin(), suitableRefs.end()), suitableRefs.end());
+}
+
+static string FindCommonPrefix(vector<string> &suitableRefs) {
+    Trie *trie = trie_create();
+    for_each(suitableRefs.begin(), suitableRefs.end(), [trie](string s) {
+        trie_add(trie, s);
+    });
+    string maxCommonPrefix = trie_get_common_prefix(trie);
+    trie_free(trie);
+    return maxCommonPrefix;
+}
+
+static string ObtainNextSuggestedSuffix(string currentPrefix, string currentSuffix, vector<string> &suitableRefs) {
+    vector<string>::iterator it;
+    if (currentSuffix.empty()) {
+        it = suitableRefs.begin();
+    }
+    else {
+        it = find(suitableRefs.begin(), suitableRefs.end(), currentPrefix + currentSuffix);
+        if (it == suitableRefs.end()) {
+            it = suitableRefs.begin();
+        } else {
+            it++;
+            if (it == suitableRefs.end()) {
+                it = suitableRefs.begin();
+            }
+        }
+    }
+    return DropPrefix(*it, currentPrefix);
+}
+
+static void TransformCmdLine(CmdLine &cmdLine, git_repository *repo) {
+    string currentPrefix = w2mb(GetUserPrefix(cmdLine));
+    logFile << "User prefix = \"" << currentPrefix.c_str() << "\"" << endl;
+
+    vector<string> suitableRefs;
+    ObtainSuitableRefsByPrefix(repo, currentPrefix, suitableRefs);
+
+    if (suitableRefs.empty()) {
+        // TODO: support another completion options, e.g. "s/t" -> "svn/trunk"
+        logFile << "No suitable refs" << endl;
+        return;
+    }
+
+    for_each(suitableRefs.begin(), suitableRefs.end(), [](string s) {
+        logFile << "Suitable ref: " << s.c_str() << endl;
+    });
+
+    string newPrefix = FindCommonPrefix(suitableRefs);
+    logFile << "Common prefix: " << newPrefix.c_str() << endl;
+
+    if (newPrefix != currentPrefix) {
+        ReplaceUserPrefix(cmdLine, mb2w(newPrefix));
+
+    } else {
+        string currentSuffix = w2mb(GetSuggestedSuffix(cmdLine));
+        logFile << "currentSuffix = \"" << currentSuffix.c_str() << "\"" << endl;
+
+        string newSuffix = ObtainNextSuggestedSuffix(currentPrefix, currentSuffix, suitableRefs);
+        logFile << "nextSuffx = \"" << newSuffix.c_str() << "\"" << endl;
+        ReplaceSuggestedSuffix(cmdLine, mb2w(newSuffix));
+    }
+}
+
+#pragma endregion
+
+#pragma region Communication with Far
+
+static struct PluginStartupInfo Info;
+
+void WINAPI GetGlobalInfoW(struct GlobalInfo *GInfo) {
     logFile.open("plugin_log.txt");
     logFile << L"I am started" << endl;
     // TODO: add time
@@ -38,27 +266,22 @@ void WINAPI GetGlobalInfoW(struct GlobalInfo *GInfo)
 	GInfo->Author=PLUGIN_AUTHOR;
 }
 
-void WINAPI ExitFARW(const struct ExitInfo *EInfo)
-{
+void WINAPI ExitFARW(const struct ExitInfo *EInfo) {
     git_libgit2_shutdown();
 
     logFile << L"I am closed" << endl;
-    logFile.flush();
     logFile.close();
 }
 
-const wchar_t *GetMsg(int MsgId)
-{
+const wchar_t *GetMsg(int MsgId) {
 	return Info.GetMsg(&MainGuid,MsgId);
 }
 
-void WINAPI SetStartupInfoW(const struct PluginStartupInfo *psi)
-{
+void WINAPI SetStartupInfoW(const struct PluginStartupInfo *psi) {
 	Info=*psi;
 }
 
-void WINAPI GetPluginInfoW(struct PluginInfo *PInfo)
-{
+void WINAPI GetPluginInfoW(struct PluginInfo *PInfo) {
 	PInfo->StructSize=sizeof(*PInfo);
 	PInfo->Flags=PF_EDITOR;
 	static const wchar_t *PluginMenuStrings[1];
@@ -67,12 +290,6 @@ void WINAPI GetPluginInfoW(struct PluginInfo *PInfo)
 	PInfo->PluginMenu.Strings=PluginMenuStrings;
 	PInfo->PluginMenu.Count=ARRAYSIZE(PluginMenuStrings);
 }
-
-typedef struct tCmdLine {
-    wstring line;
-    int curPos;
-    int selectionStart, selectionEnd;
-} CmdLine;
 
 static CmdLine GetCmdLine() {
     int lineLen = (int)Info.PanelControl(PANEL_ACTIVE, FCTL_GETCMDLINE, 0, nullptr);
@@ -97,7 +314,7 @@ static CmdLine GetCmdLine() {
     return result;
 }
 
-static void SetCmdLine(CmdLine cmdLine) {
+static void SetCmdLine(const CmdLine &cmdLine) {
     Info.PanelControl(PANEL_ACTIVE, FCTL_SETCMDLINE, 0, (void*)cmdLine.line.c_str());
     Info.PanelControl(PANEL_ACTIVE, FCTL_SETCMDLINEPOS, cmdLine.curPos, nullptr);
     struct CmdLineSelect selection = { sizeof(selection), cmdLine.selectionStart, cmdLine.selectionEnd };
@@ -123,211 +340,46 @@ static wstring GetActivePanelDir() {
     return result;
 }
 
-static string wcstringtombstring(wstring wstr) {
-    size_t mbstrSize = wstr.length() * 2 + 1;
-    char *mbstr = (char*)malloc(mbstrSize); // this is enough for all ;) FIXME
-    bool convertedOk = (wcstombs(mbstr, wstr.c_str(), mbstrSize) != (size_t)-1);
-    if (!convertedOk) {
-        logFile << "Cannot convert string \"" << wstr.c_str() << "\" to multi-byte string :(" << endl;
-        return string("");
-    }
-    string result = string(mbstr);
-    free(mbstr);
-    return result;
-}
-
-static wstring mbstringtowcstring(string str) {
-    return wstring(str.begin(), str.end());
-}
-
-static git_repository* OpenGitRepo(wstring dir) {
-    string dirForGit = wcstringtombstring(dir);
-    if (dirForGit.length() == 0) {
-        logFile << "Bad dir for Git: " << dir.c_str() << endl;
-        return nullptr;
-    }
-
-    git_repository *repo;
-    int error = git_repository_open_ext(&repo, dirForGit.c_str(), 0, nullptr);
-    if (error < 0) {
-        const git_error *e = giterr_last();
-        logFile << "libgit2 error " << error << "/" << e->klass << ": " << e->message << endl;
-        return nullptr;
-    }
-
-    return repo;
-}
-
-static bool StartsWith(const char *str, const char *prefix) {
-    return strstr(str, prefix) == str;
-}
-
-static void FilterReference(const char *ref, const char *userPrefix, vector<string> &suitableRefs) {
-    if (StartsWith(ref, userPrefix)) {
-        suitableRefs.push_back(string(ref));
-    }
-}
-
-static void FilterReferences(const char *ref, const char *userPrefix, vector<string> &suitableRefs) {
-    const char *prefixes[] = { "refs/heads/", "refs/tags/", "refs/stash" };
-    const char *remotePrefix = "refs/remotes/";
-    for (int i = 0; i < sizeof(prefixes) / sizeof(prefixes[0]); ++i) {
-        if (StartsWith(ref, prefixes[i])) {
-            FilterReference(ref + strlen(prefixes[i]), userPrefix, suitableRefs);
-            return;
-        }
-    }
-    if (StartsWith(ref, remotePrefix)) {
-        const char *remoteRef = ref + strlen(remotePrefix);
-        FilterReference(remoteRef, userPrefix, suitableRefs);
-
-        const char *slashPtr = strchr(remoteRef, '/');
-        assert(slashPtr != nullptr);
-        FilterReference(slashPtr + 1, userPrefix, suitableRefs);
-        return;
-    }
-    logFile << "DropRefPrefix: unexpected ref = " << ref << endl;
-}
-
-static wstring GetUserPrefix(CmdLine cmdLine) {
-    int prefixStart = 0;
-    int prefixEnd;
-    if (cmdLine.curPos == cmdLine.selectionEnd) {
-        assert(cmdLine.selectionStart < cmdLine.selectionEnd);
-        prefixEnd = cmdLine.selectionStart - 1;
-    } else {
-        prefixEnd = cmdLine.curPos - 1;
-    }
-    for (int i = prefixEnd; i >= 0; --i) {
-        if (iswspace(cmdLine.line.at(i))) {
-            prefixStart = i + 1;
-            break;
-        }
-    }
-    return cmdLine.line.substr(prefixStart, prefixEnd - prefixStart + 1);
-}
-
-static wstring GetSuggestedSuffix(CmdLine cmdLine) {
-    if (cmdLine.selectionStart >= 0) {
-        return cmdLine.line.substr(cmdLine.selectionStart, cmdLine.selectionEnd - cmdLine.selectionStart);
-    } else {
-        return wstring(L"");
-    }
-}
-
-static string GetNextSuggestedSuffix(string userPrefix, string currentSuffix, vector<string> &suitableRefs) {
-    vector<string>::iterator it;
-    if (currentSuffix.empty()) {
-        it = suitableRefs.begin();
-    }
-    else {
-        it = find(suitableRefs.begin(), suitableRefs.end(), userPrefix + currentSuffix);
-        if (it == suitableRefs.end()) {
-            it = suitableRefs.begin();
-        } else {
-            it++;
-            if (it == suitableRefs.end()) {
-                it = suitableRefs.begin();
-            }
-        }
-    }
-    return (*it).substr(userPrefix.length());
-}
-
-static void InsertInCurPos(CmdLine &cmdLine, wstring str) {
-    cmdLine.line.insert((size_t)cmdLine.curPos, str);
-    cmdLine.curPos += (int)str.length();
-}
-
-static void ReplaceSelection(CmdLine &cmdLine, wstring str) {
-    if (cmdLine.selectionStart >= 0) {
-        cmdLine.line.replace(cmdLine.selectionStart, cmdLine.selectionEnd - cmdLine.selectionStart, str);
-        cmdLine.curPos = cmdLine.selectionEnd = cmdLine.selectionStart + (int)str.length();
-    } else {
-        cmdLine.selectionStart = cmdLine.curPos;
-        InsertInCurPos(cmdLine, str);
-        cmdLine.selectionEnd = cmdLine.selectionStart + (int)str.length();
-    }
-}
-
-HANDLE WINAPI OpenW(const struct OpenInfo *OInfo)
-{
+HANDLE WINAPI OpenW(const struct OpenInfo *OInfo) {
     logFile << endl << "I AM OPENED" << endl;
     if (OInfo->OpenFrom != OPEN_PLUGINSMENU) {
         logFile << "OpenW, bad OpenFrom" << endl;
         return INVALID_HANDLE_VALUE;
     }
 
-    CmdLine cmdLine = GetCmdLine();
-
-    logFile << "cmdLine = \"" << cmdLine.line.c_str() << "\"" << endl;
-    logFile << "cursor pos = " << cmdLine.curPos << endl;
-    logFile << "selection start = " << cmdLine.selectionStart << endl;
-    logFile << "selection end   = " << cmdLine.selectionEnd << endl;
-
     wstring curDir = GetActivePanelDir();
-    if (curDir.length() == 0) {
+    if (curDir.empty()) {
         logFile << "Bad current dir" << endl;
         return INVALID_HANDLE_VALUE;
     }
-
     logFile << "curDir = " << curDir.c_str() << endl;
 
     git_repository *repo = OpenGitRepo(curDir);
     if (repo == nullptr) {
         logFile << "Git repo is not opened" << endl;
-        return INVALID_HANDLE_VALUE;
+        return nullptr;
     }
 
-    string userPrefix = wcstringtombstring(GetUserPrefix(cmdLine));
-    logFile << "User prefix = \"" << userPrefix.c_str() << "\"" << endl;
-    vector<string> suitableRefs;
-    {
-        git_reference_iterator *iter = NULL;
-        int error = git_reference_iterator_new(&iter, repo);
+    CmdLine cmdLine = GetCmdLine();
 
-        git_reference *ref;
-        while (!(error = git_reference_next(&ref, iter))) {
-            FilterReferences(git_reference_name(ref), userPrefix.c_str(), suitableRefs);
-        }
-        assert(error == GIT_ITEROVER);
-    }
+    logFile << "Before transformation:" << endl;
+    logFile << "cmdLine = \"" << cmdLine.line.c_str() << "\"" << endl;
+    logFile << "cursor pos = " << cmdLine.curPos << endl;
+    logFile << "selection start = " << cmdLine.selectionStart << endl;
+    logFile << "selection end   = " << cmdLine.selectionEnd << endl;
 
-    if (suitableRefs.empty()) {
-        logFile << "No suitable refs" << endl;
-        return NULL;
-    }
-
-    // TODO: sort by date if settings
-    sort(suitableRefs.begin(), suitableRefs.end());
-    suitableRefs.erase(unique(suitableRefs.begin(), suitableRefs.end()), suitableRefs.end());
-    for_each(suitableRefs.begin(), suitableRefs.end(), [](string s) {
-        logFile << "Suitable ref: " << s.c_str() << endl;
-    });
-
-    Trie *trie = trie_create();
-    for_each(suitableRefs.begin(), suitableRefs.end(), [trie](string s) {
-        trie_add(trie, s);
-    });
-    string maxCommonPart = trie_get_common_prefix(trie);
-    logFile << "Common prefix: " << maxCommonPart.c_str() << endl;
-    trie_free(trie);
-
-    if (maxCommonPart.length() > userPrefix.length()) {
-        assert(StartsWith(maxCommonPart.c_str(), userPrefix.c_str()));
-        string extraPrefix = maxCommonPart.substr(userPrefix.length());
-        logFile << "Added prefix: " << extraPrefix.c_str() << endl;
-        InsertInCurPos(cmdLine, mbstringtowcstring(extraPrefix));
-    } else {
-        string currentSuffix = wcstringtombstring(GetSuggestedSuffix(cmdLine));
-        logFile << "currentSuffix = \"" << currentSuffix.c_str() << "\"" << endl;
-        string nextSuffix = GetNextSuggestedSuffix(userPrefix, currentSuffix, suitableRefs);
-        logFile << "nextSuffx = \"" << nextSuffix.c_str() << "\"" << endl;
-        ReplaceSelection(cmdLine, mbstringtowcstring(nextSuffix));
-    }
-    SetCmdLine(cmdLine);
-
+    TransformCmdLine(cmdLine, repo);
     git_repository_free(repo);
 
-	return NULL;
+    logFile << "After transformation:" << endl;
+    logFile << "cmdLine = \"" << cmdLine.line.c_str() << "\"" << endl;
+    logFile << "cursor pos = " << cmdLine.curPos << endl;
+    logFile << "selection start = " << cmdLine.selectionStart << endl;
+    logFile << "selection end   = " << cmdLine.selectionEnd << endl;
+
+    SetCmdLine(cmdLine);
+
+	return nullptr;
 }
+
+#pragma endregion
