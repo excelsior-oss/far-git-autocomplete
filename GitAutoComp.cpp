be3255cff053e5d8cee6379d523297c0991b5b2f
Vladimir Parfinenko <vparfinenko@excelsior-usa.com>
2016-11-23 11:34:43 +0700

    Custom refs dialog without dialog border
    We use list box border for proper showing of filtering by CtrlAltF

--- GitAutoComp.cpp
+++ GitAutoComp.cpp
@@ -89,7 +89,7 @@ void WINAPI SetStartupInfoW(const struct PluginStartupInfo *psi) {
 }
 
 intptr_t WINAPI ConfigureW(const ConfigureInfo* CfgInfo) {
-    PluginDialogBuilder Builder(Info, MainGuid, DialogGuid, MTitle, L"Config");
+    PluginDialogBuilder Builder(Info, MainGuid, ConfigDialogGuid, MTitle, L"Config");
 
     Builder.AddCheckbox(MShowDialog, &globalOptions.showDialog);
     Builder.AddCheckbox(MStripRemoteName, &globalOptions.stripRemoteName);
--- RefsDialog.cpp
+++ RefsDialog.cpp
@@ -3,8 +3,6 @@
 #include <algorithm>
 #include <cassert>
 
-#include <plugin.hpp>
-#include <DlgBuilder.hpp>
 #include "GitAutoComp.hpp"
 #include "guid.hpp"
 #include "GitAutoCompLng.hpp"
@@ -12,47 +10,145 @@
 
 using namespace std;
 
-string ShowRefsDialog(vector<string> &suitableRefs) {
-    assert(!suitableRefs.empty());
+static size_t MaxLength(const vector<string> &list) {
+    return (*max_element(list.begin(), list.end(), [](string x, string y) -> bool {
+        return x.length() < y.length();
+    })).length();
+}
 
-    size_t refsCount = suitableRefs.size();
-    wchar_t **list = new wchar_t*[refsCount];
-    for (size_t i = 0; i < refsCount; ++i) {
-        wstring wstr = mb2w(suitableRefs[i]);
-        list[i] = wcsdup(wstr.c_str());
-    }
-    int selected = 0;
-
+static SMALL_RECT GetFarRect() {
     SMALL_RECT farRect;
     Info.AdvControl(&MainGuid, ACTL_GETFARRECT, 0, &farRect);
     *logFile << "Far rect (l, t, r, b) = (" << farRect.Left << ", " << farRect.Top << ", " << farRect.Right << ", " << farRect.Bottom << ")" << endl;
-    int listBoxMaxWidth = (farRect.Right - farRect.Left + 1) - 18;
-    int listBoxMaxHeight = (farRect.Bottom - farRect.Top + 1) - 10;
+    return farRect;
+}
 
-    int maxRefLength = (int)(*max_element(suitableRefs.begin(), suitableRefs.end(), [](string x, string y) -> bool {
-        return x.length() < y.length();
-    })).length();
-    
+static FarListItem * InitializeListItems(const vector<string> &list) {
+    size_t size = list.size();
+    FarListItem *listItems = new FarListItem[size];
+    for (size_t i = 0; i < size; ++i) {
+        wstring wstr = mb2w(list[i]);
+        listItems[i].Text = (const wchar_t *)wcsdup(wstr.c_str());
+        listItems[i].Flags = LIF_NONE;
+    }
+    assert(size > 0);
+    listItems[0].Flags |= LIF_SELECTED;
+    return listItems;
+}
 
-    PluginDialogBuilder Builder(Info, MainGuid, DialogGuid, MTitle, L"Contents", nullptr, nullptr, FDLG_KEEPCONSOLETITLE);
-    Builder.AddListBox(&selected,
-        min(listBoxMaxWidth, maxRefLength + 3), min(listBoxMaxHeight, (int)refsCount - 1),
-        (const wchar_t**)list, refsCount, DIF_LISTNOBOX);
+static void FreeListItems(FarListItem *listItems, size_t size) {
+    for (size_t i = 0; i < size; ++i) {
+        free((wchar_t*)listItems[i].Text);
+    }
+    delete[] listItems;
+}
 
-    string result;
-    if (Builder.ShowDialog()) {
+typedef struct tGeometry {
+    int left;
+    int top;
+    size_t width;
+    size_t height;
+} Geometry;
+
+static pair<Geometry, Geometry> CalculateListBoxAndDialogGeometry(size_t maxLineLength, size_t linesCount) {
+    // Example of list geometry:
+    // (left padding is added inside of ListBox automatically)
+    //
+    // +-- Title --+
+    // |..bar    ..|
+    // |..feature..|
+    // +-----------+
+
+    size_t listBoxWidth = maxLineLength + 6;
+    size_t listBoxHeight = linesCount + 2;
+
+    // Example of dialog geometry:
+    //
+    // ..........
+    // ..+----+..
+    // ..|    |..
+    // ..+----+..
+    // ..........
+
+    size_t dialogWidth = listBoxWidth + 4;
+    size_t dialogHeight = listBoxHeight + 2;
+
+    // Some pretty padding for the maximum dialog:
+    SMALL_RECT farRect = GetFarRect();
+    size_t dialogMaxWidth = (farRect.Right - farRect.Left + 1) - 8;
+    size_t dialogMaxHeight = (farRect.Bottom - farRect.Top + 1) - 4;
+
+    if (dialogWidth > dialogMaxWidth) {
+        int delta = dialogWidth - dialogMaxWidth;
+        dialogWidth -= delta;
+        listBoxWidth -= delta;
+    }
+    if (dialogHeight > dialogMaxHeight) {
+        int delta = dialogHeight - dialogMaxHeight;
+        dialogHeight -= delta;
+        listBoxHeight -= delta;
+    }
+
+    Geometry list = {2, 1, listBoxWidth, listBoxHeight};
+    Geometry dialog = {-1, -1, dialogWidth, dialogHeight}; // auto centering
+    return pair<Geometry, Geometry>(list, dialog);
+}
+
+static int ShowListAndGetSelected(vector<string> &list) {
+    FarList listDesc;
+    listDesc.StructSize = sizeof(listDesc);
+    listDesc.Items = InitializeListItems(list);
+    listDesc.ItemsNumber = list.size();
+
+    FarDialogItem listBox;
+    memset(&listBox, 0, sizeof(listBox));
+    listBox.Type = DI_LISTBOX;
+    listBox.Data = GetMsg(MTitle);
+    listBox.Flags = DIF_NONE;
+    listBox.ListItems = &listDesc;
+
+    auto listAndDialogGeometry = CalculateListBoxAndDialogGeometry(MaxLength(list), list.size());
+    Geometry listGeometry = listAndDialogGeometry.first;
+    Geometry dialogGeometry = listAndDialogGeometry.second;
+
+    listBox.X1 = listGeometry.left;
+    listBox.Y1 = listGeometry.top;
+    listBox.X2 = listGeometry.left + listGeometry.width - 1;
+    listBox.Y2 = listGeometry.top + listGeometry.height - 1;
+
+    FarDialogItem *items = &listBox;
+    size_t itemsCount = 1;
+    int listBoxID = 0;
+
+    assert(dialogGeometry.left == -1 && dialogGeometry.top == -1); // auto centering
+    HANDLE dialog = Info.DialogInit(&MainGuid, &RefsDialogGuid, -1, -1, dialogGeometry.width, dialogGeometry.height, L"Contents", items, itemsCount, 0, FDLG_NONE, nullptr, nullptr);
+
+    int runResult = Info.DialogRun(dialog);
+
+    int selected;
+    if (runResult != -1) {
+        selected = Info.SendDlgMessage(dialog, DM_LISTGETCURPOS, listBoxID, nullptr);
+        assert(0 <= selected && selected < (int)listBox.ListItems->ItemsNumber);
+    } else {
+        selected = -1;
+    }
+
+    Info.DialogFree(dialog);
+    FreeListItems(listDesc.Items, listDesc.ItemsNumber);
+
+    return selected;
+}
+
+string ShowRefsDialog(vector<string> &suitableRefs) {
+    assert(!suitableRefs.empty());
+
+    int selected = ShowListAndGetSelected(suitableRefs);
+    if (selected >= 0) {
         *logFile << "Dialog succeeded. Selected = " << selected << endl;
-        assert(0 <= selected && selected < refsCount);
-        result = w2mb(wstring(list[selected]));
+        assert(0 <= selected && selected < (int)suitableRefs.size());
+        return suitableRefs[selected];
     } else {
         *logFile << "Dialog failed." << endl;
-        result = "";
+        return string("");
     }
-
-    for (size_t i = 0; i < refsCount; ++i) {
-        free(list[i]);
-    }
-    delete[] list;
-
-    return result;
 }
